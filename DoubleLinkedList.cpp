b9bfbee0daadfe144138ec88209747c82df139d6
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-04-30 16:40:21 +0700

    membuat  insertion

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -66,6 +66,13 @@ public:
             return;
         }
 
+        newNode->next = current->next;
+        newNode->prev = current;
 
+
+        if (current->next != NULL)
+            current->next->prev = newNode;
+
+        current->next = newNode;
     }
 }
