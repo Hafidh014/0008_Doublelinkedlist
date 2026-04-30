26c83b6a30c75f5f9694b506b3d79a6ee5ed16dc
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-04-30 16:39:18 +0700

    validasi duplikat

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -59,5 +59,13 @@ public:
         {
             current = current->next;
         }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
+
+
     }
 }
