2525d05f311e1a9c2b54c573e7c2cd625e778b46
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-04-30 16:48:48 +0700

    Hapus di tengah/akhir + delete

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -104,6 +104,15 @@ public:
                 if (START != NULL)
                     START->prev = NULL;
             }
+            else
+            {
+                current->prev->next = current->next;
 
+                if (current->next != NULL)
+                    current->next->prev = current->prev;
+            }
+
+            delete current;
+            cout << "Record with roll number " << rollNo << " deleted" << endl;
     }
 }
